1e8d0a7f7d5bb07b1cf169517b846367b5a7121d
Brian Teague <br.teague@gmail.com>
2025-08-02 16:38:40 -0400

    Include _hyper id for style tag

--- docs/buildHtml.js
+++ docs/buildHtml.js
@@ -1,26 +1,26 @@
 import fs from 'fs'
 import path from 'path'
-import { extractCss, app } from '../dist/index.js'
+import { extractCss, app } from './index.js'
 import { init, view } from './app.js'
 import { renderToString } from 'hyperapp-render'
 
 const appHtml = renderToString(view(init));
 
-const css = extractCss()
+const css = extractCss();
 
 const htmlDocument = `
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<title>Calculator SSR</title>
-<style>${css}</style>
-</head>
-<body>
-  <div id="app">${appHtml}</div>
-  <script type="module" src="./main.js"></script>
-</body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Calculator SSR</title>
+    <style id="_hyper">${css}</style>
+  </head>
+  <body>
+    <div id="app">${appHtml}</div>
+    <script type="module" src="./main.js"></script>
+  </body>
 </html>
 `
 
